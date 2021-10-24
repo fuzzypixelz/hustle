dc12e91147dd08d3dffa7f77537139744a7a267f
fuzzypixelz <mazouz.mahmoud@outlook.com>
2021-10-24 20:43:56 +0200

    Proper error reporting in the reformatter.
    Removed the hacky parseKDL function.

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,15 @@
 module Main where
-import KDL
+
+import qualified Data.Text                     as T
+import           KDL                            ( document
+                                                , errorBundlePretty
+                                                , parse
+                                                )
 
 main :: IO ()
-main = parseKDL'
+main = do
+  input <- T.pack <$> getContents
+  case parse document "" input of
+    Left  e -> putStrLn (errorBundlePretty e)
+    Right d -> print d
+
